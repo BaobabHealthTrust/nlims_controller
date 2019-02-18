abeef9b739d498125ab823b654664908e285e269
James Mwantisi <jamesmwantisi@gmail.com>
2019-02-18 14:09:58 +0200

    specimen: recommended_examination, treatment_history, sample_date

---
+++ db/migrate/20190215100241_add_other_properties_for_tb_lab_request.rb
@@ -0,0 +1,13 @@
+class AddOtherPropertiesForTbLabRequest < ActiveRecord::Migration[5.1]
+  def up
+    execute "ALTER TABLE specimen ADD recommended_examination VARCHAR(255);";
+    execute "ALTER TABLE specimen ADD treatment_history VARCHAR(255);";
+    execute "ALTER TABLE specimen ADD sample_date datetime;";
+  end
+    
+  def down
+    execute "ALTER TABLE specimen DROP recommended_examination;";
+    execute "ALTER TABLE specimen DROP treatment_history;";
+    execute "ALTER TABLE specimen DROP sample_date;";
+  end
+end
