a9fddc6f8c0fc1a27652e4abfee1e7f5d28efde3
Pachawo Bisani <phbisan@gmail.com>
2020-05-19 12:12:47 +0200

    Fix merge conflicts

--- app/models/testtype_measure.rb
+++ app/models/testtype_measure.rb
@@ -1,6 +1,2 @@
 class TesttypeMeasure < ApplicationRecord
-<<<<<<< HEAD
-
-=======
->>>>>>> 4b5bd5254804f4b98d01d2619c40c668d4b2a3c4
 end
--- db/schema.rb
+++ db/schema.rb
@@ -171,51 +171,6 @@ ActiveRecord::Schema.define(version: 20190111175333) do
     t.index ["specimen_status_id"], name: "index_specimen_status_trails_on_specimen_status_id"
   end
 
-  create_table "slave_orders", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "sending_facility"
-    t.string "sending_facility_resolved_to"
-    t.string "receiving_facility"
-    t.string "receiving_facility_resolved_to"
-    t.string "sample_type"
-    t.string "sample_type_resolved_to"
-    t.string "who_order_first_name"
-    t.string "who_order_last_name"
-    t.string "who_order_id"
-    t.string "who_order_phone"
-    t.string "art_start_date"
-    t.string "dispatched_date"
-    t.string "date_drawn"
-    t.string "date_received"
-    t.string "date"
-    t.string "district"
-    t.string "order_location"
-    t.string "order_location_resolved_to"
-    t.string "priority"
-    t.string "order_status"
-    t.string "order_status_resolved_to"
-  end
-
-  create_table "slave_test_results", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "id"
-    t.string "test_type"
-    t.string "measure"
-    t.string "measure_resolved_to"
-    t.string "measure_value"
-    t.string "resolving_status"
-  end
-
-  create_table "slave_tests", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "id"
-    t.string "test_type"
-    t.string "test_type_resolved_to"
-    t.string "test_status"
-    t.string "test_status_resolved_to"
-    t.string "remarks"
-    t.string "date_time_started"
-    t.string "date_time_completed"
-    t.string "resolving_status"
-  end
-
   create_table "specimen_statuses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
